b7c7873a3e7eb0a4a02ff31ab66c6891ad373bd0
Denis Campos <deniscpaula@hotmail.com>
2015-04-02 12:18:01 -0300

    funcionando
    retorna as palavras em ordem alfabética na função main para verificar
    se é anagrama.

--- verificar-anagrama.c
+++ verificar-anagrama.c
@@ -1,72 +1,70 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <string.h>
 
-char * ordAlfabetica(char * word){
+void ordAlfabetica(char * word, char * ordem){
 	int menor, x, i, n = 0;
 	int tamanho = strlen(word);
 	int original = strlen(word);
-	char ordem[50];
+
 	while(tamanho){
 		for(x = 0; word[x] != '\0'; x++){ //Analisa caracter por caracter da palavra
 			for(i = 0; word[i] != '\0'; i++){
 				if(word[x] > word[i]){
-					menor = 0;  //Não é o menor caracter
-					break; 		//Para e passa a verificar o próximo
+					menor = 0; //Não é o menor caracter
+					break; //Para e passa a verificar o próximo
 				} else {
-					menor = 1;  //Diz que o caracter é menor que os outros que foram comparados
+					menor = 1; //Diz que o caracter é menor que os outros que foram comparados
 				}
 			}
 			if(menor && n < original){ //verifica se o caracter é o menor e se o tamanho da nova palavra é menor que a original
 				ordem[n] = word[x]; //ordem recebe o menor caracter encontrado
 				n++; //incrimenta para adicionar o próximo caracter na próxima posição
-				for(i = 0; i < tamanho; i++) 
+				for(i = 0; i < tamanho; i++)
 					if(i < x)
-						word[i] = word[i];	//copia a primeira parte da palavra
+						word[i] = word[i]; //copia a primeira parte da palavra
 					else
 						word[i] = word[i + 1]; //exclui o menor caracter e copia o resto
 				word[i-1] = '\0'; //define o final da string
-			tamanho--; //reduz tamanho pois um caracter foi removido
+				tamanho--; //reduz tamanho pois um caracter foi removido
 			}
 		}
 	}
-	ordem[original] = '\0'; //Limita a nova string ao mesmo tamnho da original
-	printf("%s\n", ordem);
-	return 0; //return ordem da erro -> address of stack memory associated with local variable 'ordem'
 }
 
 int main() {
-		int x, anagrama;
-        char a[50],b[50]; 
+	int x, anagrama;
+    char a[50],b[50];
 
-        printf("Digite a primeira palavra\n"); 
-        gets(a); 
-        printf("Digite a segunda palavra\n"); 
-        gets(b);
-        
+    printf("Digite a primeira palavra\n");
+    gets(a);
+    printf("Digite a segunda palavra\n");
+    gets(b);
+    char origA[strlen(a)], origB[strlen(b)];
+    for(x = 0; a[x] != '\0'; x++)
+        origA[x] = a[x];
+    origA[x] = '\0';
+    for(x = 0; b[x] != '\0'; x++)
+        origB[x] = b[x];
+    origB[x] = '\0';
 
-        ordAlfabetica(a); //testar função que deve retornar as letras em ordem alfabética
-        ordAlfabetica(b); //testar função que deve retornar as letras em ordem alfabética
+    char ordA[strlen(a)], ordB[strlen(b)];
 
+    ordAlfabetica(a, ordA); //passa a palavra e retorna ela em ordem alfabética em ordA
+    ordAlfabetica(b, ordB); //passa a palavra e retorna ela em ordem alfabética em ordA
+   
+    //abaixo comparação dos retornos
 
-        //char ordA = ord(a); //preciso armazenar aqui o retorno na função
-        //char ordB = ord(b); //preciso armazenar aqui o retorno na função
-
-
-        //abaixo comparação dos retornos
-
-        /*if(strlen(a) != strlen(b)) //se não tem o mesmo tamanho, não são anagramas
-        	printf("%s não é anagrama de %s.", a, b);
-        else {
-	        for(x = 0; ordA[x] != '\0'; x++){
-	        	if (ordA[x] != ordB[x]){ //compara posição com posição da palavra em ordem alfabética
-	        		anagrama = 0; // se entrar no if não é anagrama, pois há caracteres diferentes
-	        		printf("Não é anagrama\n");
-	        		break;
-	        	}
-	        }
-	        if(anagrama)
-	        	printf("%s é anagrama de %s.\n", a, b);
-	    }*/
-
-        return 0; 
-} 
+    if(strlen(a) != strlen(b)) //se não tem o mesmo tamanho, não são anagramas
+    	printf("\n%s nao eh anagrama de %s.", origA, origB);
+    else {
+        for(x = 0; ordA[x] != '\0'; x++){
+        	if (ordA[x] != ordB[x]){ //compara posição com posição da palavra em ordem alfabética
+        		anagrama = 0; // se entrar no if não é anagrama, pois há caracteres diferentes
+        		printf("\n%s nao eh anagrama de %s.\n", origA, origB);
+        		break;
+        	}
+        }
+        if(anagrama)
+        	printf("\n%s eh anagrama de %s.\n", origA, origB);
+    }
+}
