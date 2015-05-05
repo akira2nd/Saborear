363d337fb56ca99e22b49dedaee68e8df3a8e71a
Denis Campos <deniscpaula@hotmail.com>
2015-05-04 23:15:21 -0300

    Classe restaurante estruturada

--- Saborear/src/Restaurante.java
+++ Saborear/src/Restaurante.java
@@ -1,4 +1,26 @@
+import java.util.List;
+
 
 public class Restaurante {
-
+	private String nome;
+	private ContaRestaurante conta;
+	private List<Prato> pratos;
+	
+	public Restaurante(String n, ContaRestaurante c, List<Prato> p){
+		nome = n;
+		conta = c;
+		pratos = p;
+	}
+	
+	public String getNome(){
+		return nome;
+	}
+	
+	public ContaRestaurante getConta(){
+		return conta;
+	}
+	
+	public List<Prato> getPratos(){
+		return pratos;
+	}
 }
