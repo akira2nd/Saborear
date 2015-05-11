57e82776538948377fd8bf2a58c26a3d4d932c30
Denis Campos <deniscampos@users.noreply.github.com>
2015-05-11 14:07:32 -0300

    DBRestaurante com atributos e métodos declarados

--- Saborear/src/DBRestaurante.java
+++ Saborear/src/DBRestaurante.java
@@ -1,4 +1,19 @@
+import java.util.LinkedList;
+import java.util.List;
 
 public class DBRestaurante {
-
+        
+        private List<Restaurante> restaurantes;
+        
+	public DBRestaurante(){
+		restaurantes = new LinkedList<Restaurante>();
+	}
+	
+	public List<Restaurante> getRestaurantes(){
+		return restaurantes;
+	}
+	
+	public void addRestaurante(Restaurante restaurante){
+		restaurantes.add(restaurante);
+	}
 }
