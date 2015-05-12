8c5a28a47f0628bff4f6325b96c23b1f68b454ce
Denis Campos <deniscpaula@hotmail.com>
2015-05-11 21:43:22 -0300

    Método verificarExistente adicionado para usuários

--- src/DBUsuarios.java
+++ src/DBUsuarios.java
@@ -13,9 +13,18 @@ public class DBUsuarios {
 	}
 	
 	public void addUsuario(Usuario usuario){
-		usuarios.add(usuario);
+		if(verificarExistente(usuario) == false){
+			usuarios.add(usuario);
+		}
 	}
-		
+
+	public boolean verificarExistente(Usuario usuario){
+		for(Usuario u:usuarios){
+			if(u.getConta().getNomeUsuario().equals(usuario.getConta().getNomeUsuario())) return true;
+		}
+		return false;
+	}
+	
 	public void excluirConta(String nomeUsuario){
 		for(Usuario user:usuarios){
 			if(user.getConta().getNomeUsuario().equals(nomeUsuario)) usuarios.remove(user);
